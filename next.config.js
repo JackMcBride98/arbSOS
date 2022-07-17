0f46a4336f3a2a6e23ca531810bf512c222446b3
Jack McBride <mcbride.jack1@gmail.com>
2022-07-17 13:01:17 +0100

    Optimise font loading for next
    See https://github.com/vercel/next.js/discussions/25389

--- next.config.js
+++ next.config.js
@@ -3,14 +3,27 @@ const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
     const fileLoaderRule = config.module.rules.find(
-      (rule) => rule.test && rule.test.test(".svg")
+      (rule) => rule.test && rule.test.test('.svg')
     );
     fileLoaderRule.exclude = /\.svg$/;
     config.module.rules.push({
       test: /\.svg$/,
-      loader: require.resolve("@svgr/webpack"),
+      loader: require.resolve('@svgr/webpack'),
     });
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/fonts/inter-var.woff2',
+        headers: [
+          {
+            key: 'Cache-control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+    ];
   },
 };
 
---
+++ pages/_document.jsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/BlackOpsOne-Regular.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
